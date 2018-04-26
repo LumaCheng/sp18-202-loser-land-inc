6b59cffa71d9a3af5719a1c0a4759d8f9346d6e0
LumaCheng <juyun.cheng@sjsu.edu>
2018-04-25 23:48:08 -0700

    add volume observer and two singleton

--- Code/maven-wrapper/BrokenBrick/src/main/java/com/loserland/actors/Musicplayer.java
+++ Code/maven-wrapper/BrokenBrick/src/main/java/com/loserland/actors/Musicplayer.java
@@ -10,30 +10,44 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Musicplayer extends Actor
+public class Musicplayer extends Actor implements ScoreObserver
 {
 
-    GreenfootSound backgroundMusic;
-    private Config config = ConfigFactory.getInstance().getConfig(GameContext.GAME_DEFAULT_CONFIG_FILENAME);
+    private static Musicplayer instance = new Musicplayer();
 
+    private Config config = ConfigFactory.getInstance().getConfig(GameContext.GAME_DEFAULT_CONFIG_FILENAME);
+    GreenfootSound backgroundMusic = new GreenfootSound(config.get(GameContext.GAME_BACKGROUND_MUSIC));
     private GreenfootImage button = new GreenfootImage(config.get(GameContext.PLAYER_PAUSE_IMG));
     private int WIDTH = config.get(Integer.class, GameContext.PLAYER_SIZE);
     private int HEIGHT = config.get(Integer.class, GameContext.PLAYER_SIZE);
+    private PlayState playState = new PlayState();
+    private PauseState pauseState = new PauseState();;
+//    int volume;
 
     private State state;
 
 
-    public Musicplayer(GreenfootSound bg) {
-        backgroundMusic = bg;
-        backgroundMusic.playLoop();
+    public Musicplayer() {
+        //backgroundMusic.playLoop();
         button.scale(WIDTH,HEIGHT);
         // display on screen
         setImage(button);
         state = null;
     }
- 
-    public void act() {        
 
+    public static Musicplayer getInstance(){
+        return instance;
+    }
+ 
+    public void act() {
+        if(Greenfoot.mouseClicked(this)){
+            if(backgroundMusic.isPlaying()){
+                pauseState.doAction(this);
+            }
+            else{
+                playState.doAction(this);
+            }
+        }
     }
 
     public void change() {
@@ -50,6 +64,14 @@ public class Musicplayer extends Actor
             setImage(button);
             backgroundMusic.playLoop();
         }
+    }
+
+    public void update(int v){
+        backgroundMusic.setVolume(v);
+    }
+
+    public boolean isPlaying(){
+        return backgroundMusic.isPlaying();
     }
 
 
